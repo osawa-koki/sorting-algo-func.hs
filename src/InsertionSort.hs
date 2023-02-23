370c7fbc7e4f19c2b891ca5bb7d3fa82b6e64d8f
osawa-koki <github@osawa.dev>
2023-02-23 21:01:04 +0900

    小さな修正。

--- src/InsertionSort.hs
+++ src/InsertionSort.hs
@@ -7,6 +7,6 @@ insertionSort [] = []
 insertionSort (x:xs) = insert x (insertionSort xs)
   where
     insert y [] = [y]
-    insert y l@(x':xs')
-        | y <= x' = y : l
-        | otherwise = x' : insert y xs'
+    insert y l@(x:xs)
+        | y <= x = y : l
+        | otherwise = x : insert y xs
