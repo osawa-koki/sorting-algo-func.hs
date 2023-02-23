454c5005b4d962855a0bbfe11c0698070fd94604
osawa-koki <github@osawa.dev>
2023-02-23 20:31:26 +0900

    テンプレート。

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,19 @@ module Main (main) where
 
 import Lib
 
+bubbleSort :: Ord a => [a] -> [a]
+bubbleSort xs = go (length xs) xs
+  where
+    go 0 ys = ys
+    go n ys = go (n - 1) $ bubble ys
+    bubble [] = []
+    bubble [x] = [x]
+    bubble (x:y:xs)
+        | x > y = y : bubble (x:xs)
+        | otherwise = x : bubble (y:xs)
+
 main :: IO ()
-main = someFunc
+main = do
+    let arr = [9, 3, 0, 6, 1, 8, 7, 2, 4, 5]
+    let sortedArr = bubbleSort arr
+    putStrLn $ unwords $ map show sortedArr
